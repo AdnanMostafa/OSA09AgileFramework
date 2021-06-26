a7a386b3938bf7729a91dfe697a588ef442ec4b9
mahfuz <20mahfuzahmed20@gmail.com>
2021-06-25 23:40:38 -0400

    adding Base Browser AppContains

--- src/main/java/test/Browser.java
+++ src/main/java/test/Browser.java
@@ -1,5 +1,86 @@
 package test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Browser {
+	
+	static WebDriver dr=null;
+	static String basePath=System.getProperty("user.dir");
+	
+	public static WebDriver openBrowser(String browser) {
+		
+
+		if(System.getProperty("os.name").toLowerCase().contains("windows")) {
+			getBrowserForWindows(browser);
+		}else {
+			getBrowserForMac(browser);
+			
+		}
+		 
+		return dr;
+	}
+	/*public static void getBrowserForWindows(String browser) {
+		if(browser.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", basePath+"\\src\\test\\resources\\drivers\\windows\\geckodriver.exe");
+			 dr=new FirefoxDriver();
+		}else if(browser.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", basePath+"\\src\\test\\resources\\drivers\\windows\\chromedriver.exe");
+			 dr= new ChromeDriver();
+			 dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		}else {
+
+			System.setProperty("webdriver.gecko.driver", basePath+"\\src\\test\\resources\\drivers\\windows\\geckodriver.exe");
+			 dr=new FirefoxDriver();
+		
+			System.out.println("You have entered "+ browser+" that does not exist. Please either chrome or firefox");
+		}
+		
+	}
+	public static void getBrowserForMac(String browser) {
+		if(browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\drivers\\windows\\chromedriver.exe");
+			dr = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/src/test/resources/drivers/mac/geckodriver");
+			dr = new FirefoxDriver();
+		}else {
+			System.out.println("Please set the executable browser path");
+		}
+	}*/
+	
+	//  bonigarcia
+	  
+	  public static void getBrowserForWindows(String browser) {
+		
+		if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			dr = new ChromeDriver();
+		}else if (browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			dr = new FirefoxDriver();
+		}else {
+			System.out.println("The browser path is not executeble");
+		}
+		
+	}
+	
+	public static void getBrowserForMac(String browser) {
+		if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			dr = new ChromeDriver();
+		}else if (browser.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			dr = new FirefoxDriver();
+		}else {
+			System.out.println("The browser path is not executeble");
+		}
+	}
+	
+
+
 
 }
